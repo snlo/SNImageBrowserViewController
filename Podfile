e6617444da1ad13a4e739b2ac429ea0ad48eacfe
snlo <snloveydus@sina.com>
2018-05-16 14:23:04 +0800

    first commit

---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '8.0'
+
+inhibit_all_warnings!
+
+target 'SNImageBrowserViewController' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  pod 'pop'
+  pod 'SDWebImage'
+  pod 'ReactiveObjC'
+  pod 'SNTool'
+
+  target 'SNImageBrowserViewControllerTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SNImageBrowserViewControllerUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ SNImageBrowserViewController/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  SNImageBrowserViewController
+//
+//  Created by snlo on 2018/5/16.
+//  Copyright © 2018年 snlo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import <SNTool.h>
+
+#import "UIImageView+SNImageBrowserViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    [self.imageView sn_browsingWithImageNameArray:@[@"test"] viewArray:@[self.imageView]];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
